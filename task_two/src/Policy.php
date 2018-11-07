06300c713741e05b69fd12ab5317dcb4d5fa78a3
Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>
2018-11-08 05:15:24 +0800

    refactor calculator class
    Signed-off-by: Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>

--- task_two/src/Policy.php
+++ task_two/src/Policy.php
@@ -4,14 +4,41 @@ namespace Insly\Calculator;
 
 class Policy implements Contracts\PolicyInterface
 {
+    /**
+     * Base Price
+     * 
+     * @var float
+     */
     protected $basePrice;
 
+    /**
+     * Commission
+     * 
+     * @var float
+     */
     protected $commission;
 
+    /**
+     * Tax
+     * 
+     * @var float
+     */
     protected $tax;
 
+    /**
+     * Grand total
+     * 
+     * @var float
+     */
     protected $grandTotal;
 
+    /**
+     * Initialize installment instance
+     * 
+     * @param float $basePrice
+     * @param float $commission
+     * @param float $tax
+     */
     public function __construct(float $basePrice, float $commission, float $tax)
     {
         $this->basePrice = $basePrice;
@@ -20,26 +47,54 @@ class Policy implements Contracts\PolicyInterface
         $this->grandTotal = $this->caclculatedGrandTotal($basePrice, $commission, $tax);
     }
 
+    /**
+     * Get base price
+     * 
+     * @return float
+     */
     public function getBasePriceAmount()
     {
         return $this->basePrice;
     }
 
+    /**
+     * Get commission
+     * 
+     * @return float
+     */
     public function getCommissionAmount()
     {
         return $this->commission;
     }
 
+    /**
+     * Get tax
+     * 
+     * @return float
+     */
     public function getTaxAmount()
     {
         return $this->tax;
     }
 
+    /**
+     * Get grand total
+     * @return float
+     */
     public function getGrandTotalAmount()
     {
         return $this->grandTotal;
     }
 
+    /**
+     * Calculate grand total amount
+     * 
+     * @param float $basePrice
+     * @param float $commission
+     * @param float $tax
+     * 
+     * @return float
+     */
     protected function caclculatedGrandTotal(float $basePrice, float $commission, float $tax)
     {
         return $basePrice + $commission + $tax;
