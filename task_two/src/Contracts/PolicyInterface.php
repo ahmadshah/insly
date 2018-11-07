89598a172692a30678b9a4db1c20be439ec514d2
Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>
2018-11-08 05:16:27 +0800

    refactor calculator class
    Signed-off-by: Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>

--- task_two/src/Contracts/PolicyInterface.php
+++ task_two/src/Contracts/PolicyInterface.php
@@ -4,11 +4,30 @@ namespace Insly\Calculator\Contracts;
 
 interface PolicyInterface
 {
+    /**
+     * Get base price
+     * 
+     * @return float
+     */
     public function getBasePriceAmount();
 
+    /**
+     * Get commission
+     * 
+     * @return float
+     */
     public function getCommissionAmount();
 
+    /**
+     * Get tax
+     * 
+     * @return float
+     */
     public function getTaxAmount();
 
+    /**
+     * Get grand total
+     * @return float
+     */
     public function getGrandTotalAmount();
 }
